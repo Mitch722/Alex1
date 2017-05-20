7481d693082b2bf956468c31214e002154ef55f1
mitch722 <alex.l.mitchell@btinternet.com>
2017-05-20 21:05:54 +0100

    first draft

---
+++ window_close.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 01 12:04:16 2016
+
+@author: Alexander
+"""
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+#----------------------------------------------------------------------
+
+class MainWindow:
+
+    #----------------
+
+    def __init__(self, main):
+
+        # canvas for image
+        self.canvas = Canvas(main, width=400, height=500)
+        self.canvas.grid(row=0, column=0)
+
+        # images
+        self.my_images = img = ImageTk.PhotoImage(file = "test.gif")
+
+        # set first image on canvas
+        self.image_on_canvas = self.canvas.create_image(0, 0, anchor = NW, image = self.my_images)
+
+        # button to change image
+        self.button = Button(main, text="Change", command=self.onButton)
+        self.button.grid(row=1, column=0)
+
+
+    #----------------
+
+    def page(self, main):
+
+        # canvas for image
+        self.canvas = Canvas(main, width=400, height=500)
+        self.canvas.grid(row=0, column=0)
+
+        # images
+        self.my_images = img = ImageTk.PhotoImage(file = "test.gif")
+
+        # set first image on canvas
+        self.image_on_canvas = self.canvas.create_image(0, 0, anchor = SW, image = self.my_images)
+
+        # button to change image
+        self.button = Button(main, text="Change", command=self.onButton)
+        self.button.grid(row=1, column=0)
+
+
+    def onButton(self):
+        #self.page(root)
+        self.canvas.destroy()
+        self.page(root)
+        # next image
+        
+
+#----------------------------------------------------------------------
+
+root = Tk()
+MainWindow(root)
+root.mainloop()
