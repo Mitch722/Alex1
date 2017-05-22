be78cfd6cf392ea9789d3fb2286810d6731d238c
mitch722 <alex.l.mitchell@btinternet.com>
2017-05-22 21:45:58 +0100

    updated buttons

--- window_close.py
+++ window_close.py
@@ -40,21 +40,46 @@ class MainWindow:
         self.canvas.grid(row=0, column=0)
 
         # images
+        self.my_images = img = ImageTk.PhotoImage(file = "ball2.gif")
+
+        # set first image on canvas
+        self.image_on_canvas = self.canvas.create_image(0, 0, anchor = NW, image = self.my_images)
+
+        # button to change image
+        self.button = Button(main, text="ball1", command=self.onButton1 )
+        self.button.grid(row=1, column=0)
+
+
+    def page2(self, main):
+
+        # canvas for image
+        self.canvas = Canvas(main, width=400, height=500)
+        self.canvas.grid(row=0, column=0)
+
+        # images
         self.my_images = img = ImageTk.PhotoImage(file = "ball1.gif")
 
         # set first image on canvas
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor = NW, image = self.my_images)
 
         # button to change image
-        self.button = Button(main, text="Change", command=self.onButton)
+        self.button = Button(main, text="ball2", command=self.onButton)
         self.button.grid(row=1, column=0)
+
 
 
     def onButton(self):
         
         self.canvas.destroy()
+        self.button.destroy()
         self.page(root)
         # next image
+        
+    def onButton1(self):
+        
+        self.canvas.destroy()
+        self.button.destroy()
+        self.page2(root)
         
 
 #----------------------------------------------------------------------
